814df090aaeda62ad5b6a486df4d5d3f35297c99
Timur Kerimov <tim-online@mail.ru>
2025-02-02 15:36:42 +0300

    Add committer

--- src/application/interactors/register_user.py
+++ src/application/interactors/register_user.py
@@ -1,5 +1,4 @@
-from sqlalchemy.ext.asyncio import AsyncSession
-
+from application.interfaces.commiter import Committer
 from application.interfaces.password_hasher import PasswordHasher
 from application.interfaces.token_processor import TokenProcessor
 from application.interfaces.user_repository import UserRepository
@@ -10,14 +9,14 @@ class RegisterUserInteractor:
     def __init__(
         self,
         user_repository: UserRepository,
+        commiter: Committer,
         password_hasher: PasswordHasher,
         token_processor: TokenProcessor,
-        session: AsyncSession,
     ) -> None:
         self.user_repository = user_repository
+        self.committer = commiter
         self.password_hasher = password_hasher
         self.token_processor = token_processor
-        self.session = session
 
     async def __call__(self, email: str, password: str) -> str:
         existing_user = await self.user_repository.get_by_email(email)
@@ -30,5 +29,5 @@ class RegisterUserInteractor:
             hashed_password=hashed_password,
         )
         created_user = await self.user_repository.create(new_user)
-        await self.session.commit()
+        await self.committer.commit()
         return self.token_processor.create_access_token(created_user.id)
---
+++ src/infrastructure/database/commiter.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from application.interfaces.commiter import Committer
+
+
+class SQLAlchemyCommitter(Committer):
+    def __init__(self, session: AsyncSession) -> None:
+        self.session = session
+
+    async def commit(self) -> None:
+        await self.session.commit()
+
+    async def flush(self) -> None:
+        await self.session.flush()
+
+    async def rollback(self) -> None:
+        await self.session.rollback()
